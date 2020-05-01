0eba663353b99f060201e32a0beb8890264e14ad
Claudio Pizzolato <claudio.pizzolato@griffithuni.edu.au>
2020-05-01 14:24:54 +1000

    Add muscle accuracy test

--- test/testLloyd2003Muscle.cpp
+++ test/testLloyd2003Muscle.cpp
@@ -1,5 +1,4 @@
-#include "ceinms2/ExponentialActivation.h"
-#include "ceinms2/Lloyd2019Muscle.h"
+#include "ceinms2/Lloyd2003Muscle.h"
 #include "ceinms2/Curve.h"
 #include "ceinms2/DataTable.h"
 #include <cmath>
@@ -8,14 +7,11 @@
 #include <filesystem>
 #include <map>
 #include <algorithm>
+#include <numeric>
 using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
-
-/*
-This should become a test to assess RMSE of the model
-*/
 
 constexpr double pi = 3.14159265358979323846;
 using DoubleT = double;
@@ -75,65 +71,8 @@ static ceinms::DataTable<T> fillFromCSV(const string filename,
     return data;
 }
 
-/*
-void printStates(ceinms::Lloyd2019Muscle& muscle) {
 
-        ofstream outF("out.csv");
-        outF << "calcFiberForce, activation, activeForce, dampingForce,
-fiberForce,fiberLength, musculotendonLength," "normalisedFiberLength,
-normalisedFiberLengthAtT, normalisedFiberVelocity, optimalFiberLengthAtT,"
-                "passiveForce, pennationAngle, tendonLength, tendonStrain, fa,
-fv, fp" << endl; DoubleT flStart = 0.1, flStop = 1.9; const unsigned N = 100;
-        DoubleT dx = (flStop - flStart) / N;
-        for (int i{ 0 }; i < N; ++i) {
-                outF << muscle.calculateFiberForce(1., (i+1)*dx, 0.) << ", ";
-                outF << muscle.updStates().activation << ", ";
-                outF << muscle.updStates().activeForce << ", ";
-                outF << muscle.updStates().dampingForce << ", ";
-                outF << muscle.updStates().fiberForce << ", ";
-                outF << muscle.updStates().fiberLength << ", ";
-                outF << muscle.updStates().musculotendonLength << ", ";
-                outF << muscle.updStates().normalisedFiberLength << ", ";
-                outF << muscle.updStates().normalisedFiberLengthAtT << ", ";
-                outF << muscle.updStates().normalisedFiberVelocity << ", ";
-                outF << muscle.updStates().optimalFiberLengthAtT << ", ";
-                outF << muscle.updStates().passiveForce << ", ";
-                outF << muscle.updStates().pennationAngle << ", ";
-                outF << muscle.updStates().tendonLength << ", ";
-                outF << muscle.updStates().tendonStrain << ", ";
-                outF << muscle.updStates().fa << ", ";
-                outF << muscle.updStates().fv << ", ";
-                outF << muscle.updStates().fp << endl;
-
-        }
-        outF.close();
-}
-*/
-
-
-void printSolutionSpace(ceinms::Lloyd2019Muscle &muscle) {
-    std::ofstream outF("outSolutionSpace.csv");
-    DoubleT flStart = 0.5, flStop = 1.5, fvStart = -1, fvStop = 1;
-    const unsigned N = 100;
-    DoubleT dFl = (flStop - flStart) / N;
-    DoubleT dFv = (fvStop - fvStart) / N;
-    for (unsigned iFl(0); iFl < N; ++iFl)
-        for (unsigned iFv(0); iFv < N; ++iFv) {
-            DoubleT fl = flStart + dFl * iFl;
-            DoubleT fv = fvStart + dFv * iFv;
-            DoubleT u =
-                muscle.calculateFiberForce(1., fl, fv, muscle.getParameters());
-            DoubleT t =
-                muscle.calculateTendonForce(2, fl, muscle.getParameters());
-            outF << fl << ", ";
-            outF << fv << ", ";
-            outF << u << ", ";
-            outF << t << ", ";
-            outF << u - t << endl;
-        }
-}
-
-void printStep(ceinms::Lloyd2019Muscle &muscle) {
+void printStep(ceinms::Lloyd2003Muscle &muscle) {
     std::ofstream outF("outStepResponse.csv");
     DoubleT mtuLStart = 2.1, mtuLStop = 2.3, activation = 0.5;
     muscle.setActivation(activation);
@@ -156,176 +95,9 @@ void printStep(ceinms::Lloyd2019Muscle &muscle) {
     }
 }
 
-/*
-void testAutoDiff(ceinms::Lloyd2019Muscle& muscle) {
-        dual a = 1.;
-        dual fl = 1.;
-        dual fv = 0;
-        dual u = muscle.calculateFiberForce(1, 1, 0);
-        cout << u << endl;
-        dual dudfl = derivative([&](auto x1, auto x2, auto x3) {return
-muscle.calculateFiberForce(x1, x2, x3); }, wrt(fl), a, fl, fv); cout <<
-"automatic derivative: " << dudfl << endl;
-
-        dual dx = 0.0001;
-        dual d1 = muscle.calculateFiberForce(1, 1 + dx, 0);
-        dual d2 = muscle.calculateFiberForce(1, 1 - dx, 0);
-        dual diff = (d1 - d2) / (2 * dx);
-        cout << "Numeric differentiation: " << diff << endl;
-}
-*/
-
-int main_() {
-    vector<DoubleT> act_x{ 0.4241,
-        0.4641,
-        0.5441,
-        0.6241,
-        0.7041,
-        0.7441,
-        0.8641,
-        0.9441,
-        0.9841,
-        1.0641,
-        1.1041,
-        1.2241,
-        1.7841,
-        1.8241 };
-    vector<DoubleT> act_y{ 0,
-        0.0036,
-        0.2531,
-        0.5362,
-        0.7739,
-        0.8204,
-        0.926,
-        0.9919,
-        1,
-        0.9907,
-        0.9524,
-        0.7902,
-        0.0117,
-        0 };
-
-    vector<DoubleT> pas_x{ 1, 1.116, 1.22, 1.324, 1.428, 1.61, 2 };
-    vector<DoubleT> pas_y{ 0, 0.0208, 0.0604, 0.1536, 0.3117, 0.7448, 1.8571 };
-
-    vector<DoubleT> vel_x{
-        -1, -0.624, -0.312, -0.104, 0, 0.104, 0.312, 0.624, 0.832, 0.988
-    };
-    vector<DoubleT> vel_y{
-        0, 0.0939, 0.3174, 0.6162, 1, 1.2278, 1.2972, 1.3507, 1.3823, 1.4
-    };
-
-    vector<DoubleT> ten_x{ 0,
-        0.01,
-        0.02,
-        0.06,
-        0.1,
-        0.14,
-        0.18,
-        0.24,
-        0.32,
-        0.38,
-        0.42,
-        0.44,
-        0.46,
-        0.48,
-        0.5 };
-    vector<DoubleT> ten_y{ 0,
-        0.094,
-        0.2609,
-        1.3087,
-        2.4311,
-        3.5536,
-        4.676,
-        6.3597,
-        8.6046,
-        10.2883,
-        11.4107,
-        11.9719,
-        12.5332,
-        13.0944,
-        13.6556 };
-
-    ceinms::CurveOffline act(act_x, act_y);
-    ceinms::CurveOffline pas(pas_x, pas_y);
-    ceinms::CurveOffline vel(vel_x, vel_y);
-    ceinms::CurveOffline ten(ten_x, ten_y);
-
-
-    ceinms::Lloyd2019Muscle::Parameters p;
-    p.damping = 0.1;
-    p.maxContractionVelocity = 1;
-    p.maxIsometricForce = 1;
-    p.optimalFiberLength = 1;
-    p.pennationAngleAtOptimalFiberLength = 0;
-    p.percentageChange = 0.15;
-    p.strengthCoefficient = 1;
-    p.tendonSlackLength = 1;
-    p.activeForceLengthCurve = act;
-    p.passiveForceLengthCurve = pas;
-    p.forceVelocityCurve = vel;
-    p.tendonForceStrainCurve = ten;
-
-    ceinms::Lloyd2019Muscle muscle(p);
-
-    muscle.setActivation(0.2);
-    muscle.setMusculotendonLength(1.3);
-    muscle.evaluate(0.01);
-    auto s = muscle.getState();
-
-    for (int i{ 0 }; i < 10; ++i) {
-        double interval = 0.1;
-        muscle.setActivation(0.2);
-        muscle.setMusculotendonLength(0.5+static_cast<double>(i)*interval);
-        muscle.setState(s);
-        muscle.evaluate(0.01);
-    }
-
-
-    printSolutionSpace(muscle);
-    printStep(muscle);
-
-    return 0;
-}
-
-
-int main_1() {
-    /*
-        using DoubleT = dual;
-        DoubleT a = 1;
-        a = a - 2*a;
-        cout << "it's " << a << " should be -1\n";
-
-        a = 1;
-        a = a + 2 * a;
-        cout << "it's " << a << " should be 3\n";
-
-        a = 1;
-        a = a - a;
-        cout << "it's " << a << " should be 0\n";
-
-        a = 1;
-        a = a + a;
-        cout << "it's " << a << " should be 2\n";
-
-        a = 1;
-        a = 2*a - a;
-        cout << "it's " << a << " should be 1\n";
-
-        a = 1;
-        a = 2*a + a;
-        cout << "it's " << a << " should be 3\n";
-
-        a = 1;
-        a = DoubleT{ 2 * a - a };
-        cout << "it's " << a << " should be 1\n";
-         */
-    return 0;
-}
-
 
 void runTrial(ceinms::DataTable<double> &trial) {
-    vector<DoubleT> act_x{ 0.4241,
+    const vector<DoubleT> act_x{ 0.4241,
         0.4641,
         0.5441,
         0.6241,
@@ -339,7 +111,7 @@ void runTrial(ceinms::DataTable<double> &trial) {
         1.2241,
         1.7841,
         1.8241 };
-    vector<DoubleT> act_y{ 0,
+    const vector<DoubleT> act_y{ 0,
         0.0036,
         0.2531,
         0.5362,
@@ -400,7 +172,7 @@ void runTrial(ceinms::DataTable<double> &trial) {
     ceinms::CurveOffline vel(vel_x, vel_y);
     ceinms::CurveOffline ten(ten_x, ten_y);
 
-    ceinms::Lloyd2019Muscle::Parameters p;
+    ceinms::Lloyd2003Muscle::Parameters p;
     p.damping = 0.1;
     p.maxContractionVelocity = 5.3156293513;
     p.maxIsometricForce = 1.2758948506;
@@ -418,7 +190,7 @@ void runTrial(ceinms::DataTable<double> &trial) {
         p.optimalFiberLength * std::cos(p.pennationAngleAtOptimalFiberLength)
         + p.tendonSlackLength;
     DoubleT posOffset = -2.0e-3;// m
-    ceinms::Lloyd2019Muscle muscle(p);
+    ceinms::Lloyd2003Muscle muscle(p);
 
     auto times = trial.getTimeColumn();
     auto displacements = trial.getColumn("Experimental_displacement_mm");
@@ -439,11 +211,23 @@ void runTrial(ceinms::DataTable<double> &trial) {
     trial.pushColumn("Predicted_Force_N", outForce);
 }
 
-void runMillardBenchmark() {
+/*This test runs the Maximal-Activation Biological Benchmark test described in
+Millard M, Uchida T, Seth A, Delp SL. Flexing computational muscle: modeling and simulation 
+of musculotendon dynamics. J Biomech Eng 2013; 135: 021005.
+
+In their paper, Millard et al. report mean absolute errors relative to experimentally measured 
+maximum isometric force between 3.3% and 8.9%. This test will fail if the calculated error for this same test
+is greater than 10%.
+*/
+bool runMillardBenchmark() {
+    // 1.17 is the experimentally measured maximum isometric force. See Millard et al. 2013
+    double experimentallyMeasuredFiso = 1.17;
     std::filesystem::path root(ROOT_DIR);
     std::filesystem::path dataPath{
         root / std::filesystem::path("data/millard2013/maximalActivation")
     };
+
+    bool failed(false);
     const std::map<std::string, double> trialToDisplacement{
         { "force_trial1.dat", 0.05 },
         { "force_trial2.dat", 0.1 },
@@ -459,6 +243,15 @@ void runMillardBenchmark() {
     auto times = normalisedDisplacementDataTable.getTimeColumn();
     ceinms::CurveOffline displacementCurve{ times, normalisedDisplacement };
 
+    auto squareError = [](double a, double b) {
+        double e = a - b;
+        return e * e;
+    };
+
+       auto absoluteError = [](double a, double b) {
+        return std::abs(a - b);
+    };
+
     for (auto &[trialName, disp] : trialToDisplacement) {
         auto currentTrial =
             fillFromCSV<double>((dataPath / trialName).string(), '\t', 15);
@@ -470,11 +263,26 @@ void runMillardBenchmark() {
         currentTrial.pushColumn(
             "Experimental_displacement_mm", currentDisplacementData);
         runTrial(currentTrial);
+        auto experimentalForce = currentTrial.getColumn("Experimental_Force_N");
+        auto predictedForce = currentTrial.getColumn("Predicted_Force_N");
+        
+        double sum = std::transform_reduce(experimentalForce.begin(),
+            experimentalForce.end(),
+            predictedForce.begin(),
+            0.,
+            std::plus<>(),
+            absoluteError);
+ 
+        auto absoluteRelativeError =
+            sum / experimentalForce.size() * 100. / experimentallyMeasuredFiso;
+        failed |= (absoluteRelativeError > 10.);
+        cout << "AbsoluteRelativeError @ " << disp << " mm: " <<  absoluteRelativeError << "%" << endl;
         currentTrial.print("out" + trialName);
     }
+    return failed;
 }
 
 int main() {
-    runMillardBenchmark();
-    return 0;
+    bool failed = runMillardBenchmark();
+    return failed;
 }
